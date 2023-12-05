bb4aa95a48b416d88bb992e076d56c0600165377
KrunoT <krunoslav.tomicic@gmail.com>
2023-12-05 16:50:36 +0100

    added energy sources

--- src/game/player/player.py
+++ src/game/player/player.py
@@ -6,7 +6,9 @@ from constants import SELLING_COEFFICIENT
 class Player:
     START_FUNDS = 1000
 
-    def __init__(self, player_id, name, funds=START_FUNDS, power_plants=None):
+    def __init__(
+        self, player_id, name, funds=START_FUNDS, power_plants=None, energy_sources=None
+    ):
         self.__id = player_id
         self.name = name
         self.funds = funds
@@ -17,45 +19,83 @@ class Player:
         else:
             self.power_plants = power_plants
 
+        if energy_sources is None:
+            self.energy_sources = {
+                "electric": 0,
+                "coal": 0,
+                "gas": 0,
+                "uranium": 0,
+            }
+        else:
+            self.power_plants = power_plants
+
     # This method should be called after updating any power plant (maybe at the end of a tick, hmm?)
     # To access the Player energy output use ex. 'p1.energy_output'
-    def calculate_total_energy_output(self):
+
+    def generate_energy(self):
+        energy_output = 0
+        tmp_energy_sources = self.energy_sources.copy()
+
+        for plant in self.power_plants.values():
+            for resource, consume_ammount in plant.consume_source_per_tick.items():
+                if tmp_energy_sources[resource] < consume_ammount:
+                    tmp_energy_sources[resource] = 0
+                    plant.switch_power_plant()
+                else:
+                    tmp_energy_sources[resource] -= consume_ammount
+
+            energy_output += plant.production_rate
+
+        self.energy_sources - tmp_energy_sources.copy()
+        self.energy_output = energy_output
+
+        return energy_output
+
+    def calculate_max_energy_output(self):
         energy_output = sum(
             plant.production_rate for plant in self.power_plants.values()
         )
         self.energy_output = energy_output
-        return
 
-    def buy_power_plants(self, power_plant, ammount, price_per_unit) -> bool:
+        return energy_output
+
+    def buy_power_plants(
+        self, power_plant, ammount: int, price_per_unit: float
+    ) -> list:
+        # TODO - implement correct buying price
         if not issubclass(power_plant, PowerPlant):
             print("Buying object is not a PowerPlant!")
-            return False
+            return []
 
         if self.funds < ammount * price_per_unit:
-            return False
+            return []
 
         self.funds -= ammount * price_per_unit
+
+        new_plant_ids = []
         for i in range(ammount):
             plant_id = uuid4()
             self.power_plants[plant_id] = power_plant(plant_id)
 
+            new_plant_ids.append(plant_id)
+
         self.calculate_total_energy_output()
-        return True
 
-    def sell_power_plant(self, power_plant, buying_price_per_unit):
-        # TODO - Add selling of power plant
-        # - how should the selling price be calculated?
-        if not issubclass(power_plant, PowerPlant):
-            print("Selling object is not a PowerPlant")
-            return False
+        return new_plant_ids
 
-        if self.power_plants.get(power_plant.id, None) is None:
-            print("Yout don't have a PowerPlant matching that ID!")
-            return False
+    def sell_power_plants(self, power_plant_ids: list, buying_price_per_unit: float):
+        # TODO - how should the selling price be calculated?
 
-        self.power_plants.pop(power_plant.id)
+        for plant_id in power_plant_ids:
+            if plant_id not in self.power_plants.keys():
+                print(f"You don't have a PowerPlant with ID {plant_id}!")
+                return False
 
-        self.funds += SELLING_COEFFICIENT * buying_price_per_unit
+        for plant_id in power_plant_ids:
+            self.power_plants.pop(plant_id)
+            self.funds += SELLING_COEFFICIENT * buying_price_per_unit
+
+        self.calculate_total_energy_output()
 
         return True
 
--- src/game/powerplants/power_plant.py
+++ src/game/powerplants/power_plant.py
@@ -1,19 +1,37 @@
 class PowerPlant:
-    def __init__(self, plant_id, plant_type, production_rate, tier=0):
+    def __init__(
+        self,
+        plant_id,
+        plant_type,
+        production_rate,
+        consume_source_per_tick: dict,
+        tier=0,
+    ):
         self.id = plant_id
         self.plant_type = plant_type
-        self.production_rate = (2**tier) * production_rate
+        self.plant_state = 0
+
+        self.production_rate = (2**tier) * production_rate * self.plant_state
+        self.consume_source_per_tick = consume_source_per_tick
         self.tier = tier
+
+    # Turning power plant ON/OFF
+    # TODO - add switching costs based on plant type
+    def switch_power_plant(self):
+        if self.plant_state == 0:
+            self.plant_state = 1
+        else:
+            self.plant_state = 0
 
     def upgrade_production_rate(self, new_production_rate):
         # TODO - discuss should these types of upgrades even be possible
         if new_production_rate > self.production_rate:
             self.production_rate += new_production_rate
             print(
-                f'{self.name} upgraded! Production rate increased by: {self.production_rate} MW'
+                f"{self.name} upgraded! Production rate increased by: {self.production_rate} MW"
             )
         else:
-            print('New production rate must be higher than the current rate.')
+            print("New production rate must be higher than the current rate.")
 
     def upgrade_tier(self):
         # TODO - Max tier?
@@ -21,53 +39,68 @@ class PowerPlant:
 
 
 class NuclearPowerPlant(PowerPlant):
-    production_rate = 280
+    base_production_rate = 280
+    consume_source_per_tick = {"uranium": 1, "electric": 5}
 
-    def __init__(self, plant_id, **kwargs):
+    def __init__(self, plant_id, *args, **kwargs):
         super().__init__(
-            plant_id, 'Nuclear', NuclearPowerPlant.production_rate, **kwargs
+            plant_id, "Nuclear", NuclearPowerPlant.base_production_rate, *args, **kwargs
         )
 
 
 class HydroPowerPlant(PowerPlant):
-    production_rate = 180
-
-    def __init__(self, plant_id, **kwargs):
-        super().__init__(plant_id, 'Hydro', HydroPowerPlant.production_rate, **kwargs)
-
-
-class CoalPowerPlant(PowerPlant):
-    production_rate = 120
-
-    def __init__(self, plant_id, **kwargs):
-        super().__init__(plant_id, 'Coal', CoalPowerPlant.production_rate, **kwargs)
-
-
-class GeoThermalPowerPlant(PowerPlant):
-    production_rate = 100
+    base_production_rate = 180
+    consume_source_per_tick = {"electric": 4}
 
     def __init__(self, plant_id, **kwargs):
         super().__init__(
-            plant_id, 'GeoThermal', GeoThermalPowerPlant.production_rate, **kwargs
+            plant_id, "Hydro", HydroPowerPlant.base_production_rate, **kwargs
+        )
+
+
+class CoalPowerPlant(PowerPlant):
+    base_production_rate = 120
+    consume_source_per_tick = {"coal": 1, "electric": 4}
+
+    def __init__(self, plant_id, **kwargs):
+        super().__init__(
+            plant_id, "Coal", CoalPowerPlant.base_production_rate, **kwargs
+        )
+
+
+class GeoThermalPowerPlant(PowerPlant):
+    base_production_rate = 100
+    consume_source_per_tick = {"electric": 2}
+
+    def __init__(self, plant_id, **kwargs):
+        super().__init__(
+            plant_id, "GeoThermal", GeoThermalPowerPlant.base_production_rate, **kwargs
         )
 
 
 class GasPowerPlant(PowerPlant):
-    production_rate = 80
+    base_production_rate = 80
+    consume_source_per_tick = {"gas": 3, "electric": 2}
 
     def __init__(self, plant_id, **kwargs):
-        super().__init__(plant_id, 'Gas', GasPowerPlant.production_rate, **kwargs)
+        super().__init__(plant_id, "Gas", GasPowerPlant.base_production_rate, **kwargs)
 
 
 class WindPowerPlant(PowerPlant):
-    production_rate = 50
+    base_production_rate = 50
+    consume_source_per_tick = {"electric": 1}
 
     def __init__(self, plant_id, **kwargs):
-        super().__init__(plant_id, 'Wind', WindPowerPlant.production_rate, **kwargs)
+        super().__init__(
+            plant_id, "Wind", WindPowerPlant.base_production_rate, **kwargs
+        )
 
 
 class SolarPowerPlant(PowerPlant):
-    production_rate = 20
+    base_production_rate = 20
+    consume_source_per_tick = {}
 
     def __init__(self, plant_id, **kwargs):
-        super().__init__(plant_id, 'Solar', SolarPowerPlant.production_rate, **kwargs)
+        super().__init__(
+            plant_id, "Solar", SolarPowerPlant.base_production_rate, **kwargs
+        )
