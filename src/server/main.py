7d4e49b540477fbc9b25926d306770e73388a37b
rangoiv <rangoiv@gmail.com>
2024-01-11 15:05:38 +0100

    changed names

---
+++ src/server/main.py
@@ -0,0 +1,34 @@
+import time
+from fastapi import FastAPI
+from db.db import database
+from db import run_migrations
+from contextlib import asynccontextmanager
+from config import config
+
+from routers import admin_router, users_router
+from db import Table
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await database.connect()
+    await run_migrations(database)
+
+    yield
+
+    await database.disconnect()
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+app.include_router(admin_router, prefix="/admin")
+app.include_router(users_router)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host=config['host'], port=config['port'])
