ac9ed88a22bfd3cf18947e173737931f1272a1c6
rangoiv <rangoiv@gmail.com>
2024-01-12 20:58:45 +0100

    Player api updated

--- src/server/db/table.py
+++ src/server/db/table.py
@@ -20,21 +20,21 @@ class Table:
             VALUES ({', '.join(f':{col}' for col in cols[1:])})
             RETURNING {cols[0]}"""
         values = {col: data.__getattribute__(col) for col in cols[1:]}
-        return await database.execute(query=query, values=values)
+        return await database.fetch_val(query=query, values=values)
         
     
     @classmethod
     async def update(cls, **kwargs):
         """
         Input: Updated values, row id must be provided
-        Returns ??
+        Returns: Updated rows including including rows whose values did not change
         """
         cols = [field.name for field in fields(cls)]
         assert set(kwargs.keys()).issubset(cols), f"Some columns don't exist in table {cls.table_name}"
         assert cols[0] in kwargs, "Row id wasn't provided"
         set_query = ', '.join(f'{col}=:{col}' for col in kwargs if col != cols[0])
-        query = f"UPDATE {cls.table_name} SET {set_query} WHERE {cols[0]}=:{cols[0]}"
-        return await database.execute(query, kwargs)
+        query = f"UPDATE {cls.table_name} SET {set_query} WHERE {cols[0]}=:{cols[0]} RETURNING *"
+        return await database.fetch_val(query, kwargs)
 
     @classmethod
     async def delete(cls, **kwargs):
@@ -46,8 +46,8 @@ class Table:
         assert set(kwargs.keys()).issubset(cols), f"Some columns don't exist in table {cls.table_name}"
         where = ' AND '.join(f'{col}=:{col}' for col in kwargs)
         if where: where = f" WHERE {where}"
-        query = f"DELETE FROM {cls.table_name}{where}"
-        return await database.execute(query, kwargs)
+        query = f"DELETE FROM {cls.table_name}{where} RETURNING *"
+        return await database.fetch_val(query, kwargs)
     
     @classmethod
     async def get(cls, **kwargs):
@@ -70,13 +70,19 @@ class Table:
         query, values = cls._select(**kwargs)
         result = await database.fetch_all(query, values)
         return [cls(**team) for team in result]
+    
+    @classmethod
+    async def count(cls, **kwargs):
+        query, values = cls._select(selected_cols="COUNT(*)", **kwargs)
+        result = await database.execute(query, values)
+        return result
 
     @classmethod
-    def _select(cls, **kwargs):
+    def _select(cls, selected_cols="*", **kwargs):
         cols = [field.name for field in fields(cls)]
         assert set(kwargs.keys()).issubset(
             cols), f"Some columns don't exist in table {cls.table_name}"
         where = ' AND '.join(f'{col}=:{col}' for col in kwargs)
         if where: where = f" WHERE {where}"
-        query = f"SELECT * FROM {cls.table_name}{where}"
+        query = f"SELECT {selected_cols} FROM {cls.table_name}{where}"
         return query, kwargs
--- src/server/routers/users/dependencies.py
+++ src/server/routers/users/dependencies.py
@@ -1,9 +1,20 @@
-from fastapi import HTTPException, Query
-from db import Team
+from fastapi import HTTPException, Query, Depends
+from db import Team, Player
 
 
-async def team_id(team_secret: str = Query(..., description="User secret")):
+async def team_id(team_secret: str = Query(..., description="Team secret")):
     try:
         return (await Team.get(team_secret=team_secret)).team_id
     except:
         raise HTTPException(status_code=403, detail="Invalid team_secret")
+
+
+async def player(game_id: int, player_id: int, team_id: int=Depends(team_id)):
+    try:
+        player = await Player.get(player_id=player_id)
+    except:
+        raise HTTPException(status_code=403, detail="Invalid player_id")
+    if player.team_id != team_id: raise HTTPException(403, "This player doesn't belong to your team")
+    if player.game_id != game_id: raise HTTPException(400, f"This player is in game {player.game_id}")
+    if player.is_active == False: raise HTTPException(400, f"This player is inactive")
+    return player
