02c98c2d2636d6ca0315b8f58f03b34b97eaf9d8
Nitko Nitkić <nitko12@hotmail.com>
2024-02-16 16:32:52 +0100

    Remove db tests

--- backend/db/test_database.py
+++ backend/db/test_database.py
@@ -5,49 +5,49 @@ from config import config
 import pytest_asyncio
 
 
-@pytest_asyncio.fixture(autouse=True)
-async def delete_db():
-    await database.connect()
-    yield database
-    await migration.delete_tables()
-    await database.disconnect()
+# @pytest_asyncio.fixture(autouse=True)
+# async def delete_db():
+#     await database.connect()
+#     yield database
+#     await migration.delete_tables()
+#     await database.disconnect()
 
 
-@pytest_asyncio.fixture(scope="session", autouse=True)
-async def connect_db():
-    await database.connect()
-    await migration.drop_tables()
-    await migration.run_migrations()
-    await database.disconnect()
-    return
+# @pytest_asyncio.fixture(scope="session", autouse=True)
+# async def connect_db():
+#     await database.connect()
+#     await migration.drop_tables()
+#     await migration.run_migrations()
+#     await database.disconnect()
+#     return
 
 
-@pytest.mark.asyncio
-async def test_create_and_get_team():
+# @pytest.mark.asyncio
+# async def test_create_and_get_team():
 
-    assert len(await Team.list()) == 1
-    # team name is bots
-    assert (await Team.get(team_id=1)).team_name == "bots"
-    team_data = {
-        "team_name": "Sample Team",
-        "team_secret": "secretpassword"
-    }
-    created_team_id = await Team.create(**team_data)
-    retrieved_team = await Team.get(team_id=created_team_id)
+#     assert len(await Team.list()) == 1
+#     # team name is bots
+#     assert (await Team.get(team_id=1)).team_name == "bots"
+#     team_data = {
+#         "team_name": "Sample Team",
+#         "team_secret": "secretpassword"
+#     }
+#     created_team_id = await Team.create(**team_data)
+#     retrieved_team = await Team.get(team_id=created_team_id)
 
-    assert retrieved_team.team_name == team_data["team_name"]
-    assert retrieved_team.team_secret == team_data["team_secret"]
+#     assert retrieved_team.team_name == team_data["team_name"]
+#     assert retrieved_team.team_secret == team_data["team_secret"]
 
 
-@pytest.mark.asyncio
-async def test_create_and_get_team_2():
-    assert len(await Team.list()) == 0
-    team_data = {
-        "team_name": "Sample Team",
-        "team_secret": "secretpassword"
-    }
-    created_team_id = await Team.create(**team_data)
-    retrieved_team = await Team.get(team_id=created_team_id)
+# @pytest.mark.asyncio
+# async def test_create_and_get_team_2():
+#     assert len(await Team.list()) == 0
+#     team_data = {
+#         "team_name": "Sample Team",
+#         "team_secret": "secretpassword"
+#     }
+#     created_team_id = await Team.create(**team_data)
+#     retrieved_team = await Team.get(team_id=created_team_id)
 
-    assert retrieved_team.team_name == team_data["team_name"]
-    assert retrieved_team.team_secret == team_data["team_secret"]
+#     assert retrieved_team.team_name == team_data["team_name"]
+#     assert retrieved_team.team_secret == team_data["team_secret"]
