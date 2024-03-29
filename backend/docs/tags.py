56ed95f2be03b8fda06f3f9b7787a97d472ba481
Goran Ivanković <40663576+rangoiv@users.noreply.github.com>
2024-03-30 00:39:21 +0100

    Docs (#98)

---
+++ backend/docs/tags.py
@@ -0,0 +1,35 @@
+short_description = """
+Docs for Algotrade hackathon - the biggest hackathon in Zagreb.
+
+**FAQ**: In case of any questions, ping organizers in discord or in person!
+
+**IMPORTANT**: Each team will get a team_id. Make sure to send it as a query
+parameter in all requests you send!
+"""
+
+game_desc = "Here you can see avaliable games."
+
+player_desc = "Manage players in games for your team and get their current info."
+
+market_desc = "Create buy and sell orders for resources, list orders and set price for electricity."
+
+power_plant_desc = "Buy or sell power plants and turn them on or off."
+
+tags_metadata = [
+    {
+        "name": "Games",
+        "description": game_desc,
+    },
+    {
+        "name": "Player",
+        "description": player_desc,
+    },
+    {
+        "name": "Market",
+        "description": market_desc,
+    },
+    {
+        "name": "Power plants",
+        "description": power_plant_desc
+    },
+]
