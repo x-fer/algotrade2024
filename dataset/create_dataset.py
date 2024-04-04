f443404e7a3eb9c52b0ad29cab698941b4087a6c
rangoiv <rangoiv@gmail.com>
2024-04-04 21:16:11 +0200

    new data

--- dataset/create_dataset.py
+++ dataset/create_dataset.py
@@ -3,7 +3,8 @@ import numpy as np
 import pandas as pd
 import os
 
-# list data/ dir
+
+# load weather data
 l = []
 for x in os.listdir("data"):
     if not x.endswith(".csv"):
@@ -11,142 +12,101 @@ for x in os.listdir("data"):
     if not x.startswith("1064020"):
         continue
 
-    # ignore first 2 rows
     df = pd.read_csv(f"data/{x}", skiprows=2)
-
     l.append(df)
-
 df = pd.concat(l)
 
-# Year,Month,Day,Hour,Minute to date and index
 df["Date"] = pd.to_datetime(df[["Year", "Month", "Day", "Hour", "Minute"]])
 df = df.set_index("Date")
-
-# drop columns
 df = df.drop(columns=["Year", "Month", "Day", "Hour", "Minute"])
-
-energy = pd.read_csv("data/DUQ_hourly.csv")
-
-# Datetime to date and index
-energy["Datetime"] = pd.to_datetime(energy["Datetime"])
-energy = energy.set_index("Datetime")
-
-# add 30:00 to all dates
-energy.index = energy.index + pd.Timedelta("30 minutes")
-
-# print(energy)
-
-# print(df)
-
-# Merge on index
-df = df.merge(energy, left_index=True, right_index=True)
-
-# Drop NaN
-df = df.dropna()
-
-# leave Date,Temperature,Precipitable Water,Wind Speed,Global Horizontal UV Irradiance (280-400nm)
-df = df[["Temperature", "Precipitable Water",
-         "Wind Speed", "Global Horizontal UV Irradiance (280-400nm)", "DUQ_MW"]]
-
-# Rename columns
 df = df.rename(columns={"Global Horizontal UV Irradiance (280-400nm)": "UV"})
 df = df.rename(columns={"Precipitable Water": "Rain"})
 df = df.rename(columns={"Wind Speed": "Wind"})
 df = df.rename(columns={"Temperature": "Temp"})
-df = df.rename(columns={"DUQ_MW": "Energy"})
 
-# Drop NaN
+
+# load energy data
+energy = pd.read_csv("data/DUQ_hourly.csv")
+energy["Datetime"] = pd.to_datetime(energy["Datetime"])
+energy = energy.set_index("Datetime")
+energy = energy.rename(columns={"DUQ_MW": "Energy"})
+energy.index = energy.index + pd.Timedelta("30 minutes")
+
+df = df.merge(energy, left_index=True, right_index=True)
+
+df = df[["Temp", "Rain", "Wind", "UV", "Energy"]]
+
 df = df.dropna()
 
 
 # load river data
-
 river = pd.read_csv("data/river.csv")
-# set date as index
 river["date"] = pd.to_datetime(river["date"])
 river = river.set_index("date")
+river = river.drop(columns=["status"])
+river = river.rename(columns={"river_level": "River"})
 
-# merge on index
 df = df.merge(river, left_index=True, right_index=True)
 
-# drop status
-df = df.drop(columns=["status"])
-
-# rename river_level to river
-df = df.rename(columns={"river_level": "River"})
-
 df = df.dropna()
 
+
+# load market data
 futures_df = pd.read_csv("data/futures.csv", index_col=0)
-# datetime index
-futures_df.index = pd.to_datetime(
-    futures_df.index) + pd.Timedelta("30 minutes")
+futures_df.index = pd.to_datetime(futures_df.index) + pd.Timedelta("30 minutes")
+# futures_df.rolling(3).mean()
 
-# join
 df = df.join(futures_df)
-df = df.dropna()
 
-# assert time delta is 60 minutes
+df = df.dropna()
 
 
 # split data where time jumps
-
-
 time_delta = df.index[1:] - df.index[:-1]
 df = df.iloc[1:]
 df["delta"] = time_delta
-
 # split where delta is not 60 minutes
 df["split"] = df["delta"] != pd.Timedelta("60 minutes")
-
 # cumsum split
 df["split"] = df["split"].cumsum()
-
-
 # group by split
 groups = df.groupby("split")
 
-# await database.execute('''
-#                 CREATE TABLE IF NOT EXISTS dataset_data (
-#                 dataset_data_id SERIAL PRIMARY KEY,
-#                 dataset_id INT NOT NULL,
-#                 date TIMESTAMP NOT NULL,
-#                 COAL INT NOT NULL,
-#                 URANIUM INT NOT NULL,
-#                 BIOMASS INT NOT NULL,
-#                 GAS INT NOT NULL,
-#                 OIL INT NOT NULL,
-#                 GEOTHERMAL INT NOT NULL,
-#                 WIND INT NOT NULL,
-#                 SOLAR INT NOT NULL,
-#                 HYDRO INT NOT NULL,
-#                 ENERGY_DEMAND INT NOT NULL,
-#                 MAX_ENERGY_PRICE INT NOT NULL,
-#                 FOREIGN KEY (dataset_id) REFERENCES datasets(dataset_id)
-#                 )''')
 
-
+# Prepare data for csv -> these go directly to game and sql
 def prepare_chunk(df: pd.DataFrame) -> pd.DataFrame:
-    # scale to 0 1
-    df = df * (1 / df.mean())
-
-    new_df = pd.DataFrame(columns=["date", "COAL", "URANIUM", "BIOMASS", "GAS", "OIL",
-                                   "GEOTHERMAL", "WIND", "SOLAR", "HYDRO", "ENERGY_DEMAND", "MAX_ENERGY_PRICE"])
-    new_df["date"] = df.index.to_series()
-
     def mavg_noise(box_size, size):
-        return np.convolve(np.ones(box_size) / box_size,
-                           np.random.normal(
-            0, 1, size + box_size-1),
-            mode="valid")
+        noise = np.random.normal(0, 1, size + box_size - 1)
+        return np.convolve(np.ones(box_size) / box_size, noise, mode="valid")
 
     def norm_col(col):
-        return col / col.mean()
-    
-    def linear(col):
-        end = 0.1 * len(col) / 1800
-        col = col + np.linspace(0, end, len(col))
+        return col / col[:1800].mean()
+
+    def add_linear(col):
+        end = 0.12 * len(col) / 1800
+        col = 0.2 + col + np.linspace(0, end, len(col))
         return norm_col(col)
+
+    # scale so all cols average to 1
+    df = df * (1 / df.mean())
+
+    new_df = pd.DataFrame(
+        columns=[
+            "date",
+            "COAL",
+            "URANIUM",
+            "BIOMASS",
+            "GAS",
+            "OIL",
+            "GEOTHERMAL",
+            "WIND",
+            "SOLAR",
+            "HYDRO",
+            "ENERGY_DEMAND",
+            "MAX_ENERGY_PRICE",
+        ]
+    )
+    new_df["date"] = df.index.to_series()
 
     new_df["COAL"] = mavg_noise(24, len(df)) * 0.02
     new_df["URANIUM"] = mavg_noise(24, len(df)) * 0.02
@@ -160,13 +120,15 @@ def prepare_chunk(df: pd.DataFrame) -> pd.DataFrame:
     new_df["HYDRO"] = df["River"]
 
     new_df["ENERGY_DEMAND"] = df["Energy"]
-    new_df["MAX_ENERGY_PRICE"] = norm_col(0.3 + 0.6*1/df["Energy"] + 0.1*1/df["Energy"]**2)
+    new_df["MAX_ENERGY_PRICE"] = norm_col(
+        0.3 + 0.6 * 1 / df["Energy"] + 0.1 * 1 / df["Energy"] ** 2
+    )
 
-    new_df["COAL_PRICE"] = linear(df["COAL_PRICE"])
-    new_df["URANIUM_PRICE"] = linear(df["URANIUM_PRICE"])
-    new_df["BIOMASS_PRICE"] = linear(df["BIOMASS_PRICE"])
-    new_df["GAS_PRICE"] = linear(df["GAS_PRICE"])
-    new_df["OIL_PRICE"] = linear(df["OIL_PRICE"])
+    new_df["COAL_PRICE"] = add_linear(df["COAL_PRICE"])
+    new_df["URANIUM_PRICE"] = add_linear(df["URANIUM_PRICE"])
+    new_df["BIOMASS_PRICE"] = add_linear(df["BIOMASS_PRICE"])
+    new_df["GAS_PRICE"] = add_linear(df["GAS_PRICE"])
+    new_df["OIL_PRICE"] = add_linear(df["OIL_PRICE"])
 
     for col in new_df.columns:
         # make int
@@ -192,18 +154,19 @@ def prepare_chunk(df: pd.DataFrame) -> pd.DataFrame:
     # plt.title("Dataset outputs sum")
     # plt.legend()
     # plt.show()
-
-    # plt.plot(new_df["COAL_PRICE"], label="COAL")
-    # plt.plot(new_df["URANIUM_PRICE"], label="URANIUM")
-    # plt.plot(new_df["BIOMASS_PRICE"], label="BIO")
-    # plt.plot(new_df["GAS_PRICE"], label="GAS")
-    # plt.plot(new_df["OIL_PRICE"], label="OIL")
+    # graph_df = new_df.iloc[:1800:3, :]
+    # plt.plot(graph_df["COAL_PRICE"], label="COAL")
+    # plt.plot(graph_df["URANIUM_PRICE"], label="URANIUM")
+    # plt.plot(graph_df["BIOMASS_PRICE"], label="BIO")
+    # plt.plot(graph_df["GAS_PRICE"], label="GAS")
+    # plt.plot(graph_df["OIL_PRICE"], label="OIL")
     # plt.legend()
+    # plt.show() 
 
     # plt.plot(new_df["ENERGY_DEMAND"][:1800])
     # plt.plot(new_df["MAX_ENERGY_PRICE"])
     # plt.title("PRICES")
-    # plt.show()
+    # plt.show() 
 
     return new_df
 
@@ -225,4 +188,5 @@ for name, group in groups:
     group = prepare_chunk(group)
 
     group.to_csv(
-        f"chunks/df_{len(group)}_{group.index[0]}_{group.index[-1]}.csv", index=False)
+        f"chunks/df_{len(group)}_{group.index[0]}_{group.index[-1]}.csv", index=False
+    )
--- dataset/get_futures.py
+++ dataset/get_futures.py
@@ -1,24 +1,16 @@
-from matplotlib import pyplot as plt
-import requests
 import yfinance as yf
 import pandas as pd
 
-# coal: 3000
-# uranium: 250000
-# biomass: 5000
-# gas: 7000
-# oil: 10000
 d = {
-    'COAL_PRICE': "MTF=F",  # thermal coal
-    'URANIUM_PRICE': "PA=F",  # paladium
-    'BIOMASS_PRICE': "LBS=F",  # lumber
-    'GAS_PRICE': "NG=F",  # natural gas
-    'OIL_PRICE': 'CL=F'  # crude oil
+    "COAL_PRICE": "MTF=F",  # thermal coal
+    "URANIUM_PRICE": "PA=F",  # paladium
+    "BIOMASS_PRICE": "LBS=F",  # lumber
+    "GAS_PRICE": "NG=F",  # natural gas
+    "OIL_PRICE": "CL=F",  # crude oil
 }
 
 # all available data
-df = yf.download(" ".join(d.values()),
-                 period='max')["Adj Close"]
+df = yf.download(" ".join(d.values()), period="max")["Adj Close"]
 
 # drop nan on rows
 df = df.dropna()
@@ -27,24 +19,12 @@ min_date = df.index.min()
 max_date = df.index.max()
 
 # interpolate hourly
-df = df.resample('H').interpolate()
+df = df.resample("H").interpolate()
 
-# print(df)
-
+# normalize data from 0 to 1
 for col in df.columns:
-    # normalize and plot
     df[col] = (df[col] - df[col].min()) / (df[col].max() - df[col].min())
-    # moving average
-    df[col] = df[col].rolling(window=48).mean()
-
-# drop nan on rows
-df = df.dropna()
 
 # rename and save to data
-df = df.rename(columns={
-    v: k for k, v in d.items()
-})
-df.to_csv('data/futures.csv')
-
-# df.plot()
-# plt.show()
+df = df.rename(columns={v: k for k, v in d.items()})
+df.to_csv("data/futures.csv")
