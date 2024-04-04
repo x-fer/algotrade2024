b4fe15f06f3f8187f380c79248b042c484192eee
Zvonimir Haramustek <nitko12@hotmail.com>
2024-04-04 23:34:50 +0200

    Socket (#102)
    * Add bot3
    
    * Add graphs socket
    
    * Sockets
    
    * Sockets

--- backend/1.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from matplotlib import pyplot as plt
-
-df = pd.read_csv('data-1710693841401.csv')
-
-df[df["resource"] == "COAL"]["market"].plot()
-plt.show()
--- backend/routers/admin/game.py
+++ backend/routers/admin/game.py
@@ -1,14 +1,19 @@
+from collections import defaultdict
 from dataclasses import dataclass
+import dataclasses
 from datetime import datetime
-from fastapi import APIRouter, HTTPException
-from model import Game, Datasets, Player
+import json
+from fastapi import APIRouter, HTTPException, WebSocket
+from websockets.exceptions import ConnectionClosedOK
+from model import Game, Datasets, Player, DatasetData, Market, Order, OrderStatus, OrderSide
 from game.bots import Bots
 from pydantic import BaseModel
 from datetime import datetime
 from typing import List
 from model.team import Team
 from routers.model import SuccessfulResponse
-from db import limiter
+from db import limiter, database
+import asyncio
 
 
 router = APIRouter()
@@ -134,3 +139,102 @@ async def game_networth(game_id: int) -> List[NetworthData]:
         })
 
     return team_networths
+
+
+@router.websocket("/game/{game_id}/dashboard/graphs")
+@limiter.exempt
+async def dashboard(websocket: WebSocket, game_id: int):
+    await websocket.accept()
+
+    try:
+        while True:
+            game = await Game.get(game_id=game_id)
+
+            current_tick = game.current_tick
+
+            if current_tick == 0:
+                await asyncio.sleep(game.tick_time / 1000)
+                continue
+
+            dataset = (await DatasetData.list_by_game_id_where_tick(
+                game.dataset_id, game.game_id, current_tick - 1, current_tick - 1))[0]
+
+            dataset = dataclasses.asdict(dataset)
+
+            all_prices = await Market.list_by_game_id_where_tick(
+                game_id=game.game_id,
+                min_tick=current_tick - 1,
+                max_tick=current_tick - 1,
+            )
+
+            all_prices = [dataclasses.asdict(price) for price in all_prices]
+
+            await websocket.send_json(json.dumps({
+                **dataset,
+                "prices": all_prices
+            }, default=str))
+
+            await asyncio.sleep(game.tick_time / 1000)
+    except ConnectionClosedOK:
+        pass
+
+
+@router.websocket("/game/{game_id}/dashboard/players")
+@limiter.exempt
+async def dashboard(websocket: WebSocket, game_id: int):
+    await websocket.accept()
+
+    try:
+        while True:
+            game = await Game.get(game_id=game_id)
+
+            current_tick = game.current_tick
+
+            if current_tick == 0:
+                await asyncio.sleep(game.tick_time / 1000)
+                continue
+
+            players = await Player.list(game_id=game_id)
+
+            networths = {
+                player.player_id: (await player.get_networth(game))["total"] for player in players}
+
+            players = [{**dataclasses.asdict(player),
+                        "networth": networths[player.player_id]
+                        } for player in players]
+
+            await websocket.send_json(json.dumps({
+                "current_tick": current_tick,
+                "players": players,
+            }, default=str))
+
+            await asyncio.sleep(game.tick_time / 1000)
+    except ConnectionClosedOK:
+        pass
+
+
+@router.websocket("/game/{game_id}/dashboard/orderbooks")
+@limiter.exempt
+async def dashboard(websocket: WebSocket, game_id: int):
+    await websocket.accept()
+
+    try:
+        while True:
+            game = await Game.get(game_id=game_id)
+
+            orders = await Order.list(game_id=game_id, order_status=OrderStatus.ACTIVE)
+
+            orders = [dataclasses.asdict(order) for order in orders]
+
+            orders_by_resource = defaultdict(
+                lambda: {str(OrderSide.BUY): [], str(OrderSide.SELL): []})
+
+            for order in orders:
+                orders_by_resource[str(order["resource"])
+                                   ][str(order["order_side"])].append(order)
+
+            await websocket.send_json(json.dumps(orders_by_resource, default=str))
+
+            await asyncio.sleep(game.tick_time / 1000)
+    except ConnectionClosedOK:
+        pass
